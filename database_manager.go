dca7a6f05ca3cf48b6ddcb68e25423ebce55cc15
Muhammet Ali Aygün <109526680+IrohAmca@users.noreply.github.com>
2024-05-29 16:07:16 +0300

    added post database

--- database_manager.go
+++ database_manager.go
@@ -3,21 +3,29 @@ package main
 import (
 	"database/sql"
 	"fmt"
+
 	_ "github.com/mattn/go-sqlite3"
 )
 
 var userID int
-var database *sql.DB
+var user_db *sql.DB
 
 func createDatabase() {
-	database, _ = sql.Open("sqlite3", "./database.db")
+	user_db, _ = sql.Open("sqlite3", "./user.db")
+	usr_statement, _ := user_db.Prepare("CREATE TABLE IF NOT EXISTS people (id INTEGER PRIMARY KEY, username TEXT, email TEXT, password TEXT)")
+	usr_statement.Exec()
 
-	statement, _ := database.Prepare("CREATE TABLE IF NOT EXISTS people (id INTEGER PRIMARY KEY, username TEXT, email TEXT, password TEXT)")
-	statement.Exec()
+	post_db, _ := sql.Open("sqlite3", "./post.db")
+	post_statement, _ := post_db.Prepare("CREATE TABLE IF NOT EXISTS posts (id INTEGER PRIMARY KEY, title TEXT, content TEXT, author TEXT, category TEXT, date TEXT)")
+	post_statement.Exec()
+
+	post_db.Exec("ALTER TABLE posts ADD COLUMN user_id INTEGER")
+	post_db.Exec("UPDATE posts SET user_id = ?", userID)
+
 }
 
 func updateUserID() {
-	rows, err := database.Query("SELECT MAX(id) FROM people")
+	rows, err := user_db.Query("SELECT MAX(id) FROM people")
 	if err != nil {
 		fmt.Println("Error querying data:", err)
 		return
@@ -38,7 +46,7 @@ func updateUserID() {
 
 func Query_email(email string) (string, string, error) {
 	var email_, password string
-	row := database.QueryRow("SELECT email, password FROM people WHERE email = ?", email)
+	row := user_db.QueryRow("SELECT email, password FROM people WHERE email = ?", email)
 	err := row.Scan(&email_, &password)
 	if err != nil {
 		if err == sql.ErrNoRows {
@@ -51,7 +59,7 @@ func Query_email(email string) (string, string, error) {
 
 func Query(id int) (string, string, error) {
 	var email, password string
-	row := database.QueryRow("SELECT email, password FROM people WHERE id = ?", id)
+	row := user_db.QueryRow("SELECT email, password FROM people WHERE id = ?", id)
 	err := row.Scan(&email, &password)
 	if err != nil {
 		if err == sql.ErrNoRows {
@@ -64,7 +72,7 @@ func Query(id int) (string, string, error) {
 
 func insertData(id int, username, email, password string) {
 	hashedPassword := hashPassword(password)
-	statement, err := database.Prepare("INSERT OR IGNORE INTO people (id, username, email, password) VALUES (?, ?, ?, ?)")
+	statement, err := user_db.Prepare("INSERT OR IGNORE INTO people (id, username, email, password) VALUES (?, ?, ?, ?)")
 	if err != nil {
 		fmt.Println("Error preparing statement:", err)
 		return
@@ -81,7 +89,7 @@ func insertData(id int, username, email, password string) {
 }
 
 func WriteAllData() {
-	rows, err := database.Query("SELECT id, username, email, password FROM people")
+	rows, err := user_db.Query("SELECT id, username, email, password FROM people")
 	if err != nil {
 		fmt.Println("Error querying data:", err)
 		return
@@ -102,6 +110,6 @@ func WriteAllData() {
 
 /*
 func deleteData(id int) {
-	statement, _ := database.Prepare("DELETE FROM people WHERE id = ?")
+	statement, _ := user_db.Prepare("DELETE FROM people WHERE id = ?")
 	statement.Exec(id)
 }*/
